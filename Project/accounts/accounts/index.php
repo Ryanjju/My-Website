949285bd9d00723b8726ee97ed14c28a8aedcf40
Ryan <113769772+Ryanjju@users.noreply.github.com>
2023-09-20 19:15:08 +0200

    Update index.php

--- Project/accounts/accounts/index.php
+++ Project/accounts/accounts/index.php
@@ -2,7 +2,17 @@
 <!DOCTYPE html>
 <html lang="de">
 
-<?php include '../../head_main.php' ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Benutzer:
+        <?php echo $_SESSION['username']; ?>
+    </title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/pannels.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="/js/hamburger.js"></script>
+</head>
 
 <body>
     <?php include '../../head.php';?>
